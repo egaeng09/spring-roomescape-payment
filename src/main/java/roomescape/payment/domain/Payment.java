816e30cbe2413c77cc8a087433fb180d285057e7
egaeng09 <egaeng09@gmail.com>
2025-06-01 06:04:31 +0900

    feat: Payment 검증 로직 수정

--- src/main/java/roomescape/payment/domain/Payment.java
+++ src/main/java/roomescape/payment/domain/Payment.java
@@ -28,28 +28,29 @@ public class Payment {
     private String approvedAt;
 
     private static Payment of(final Long id, final String paymentKey, final String orderId,
-        final Long amount, final String requestedAt, final String approvedAt) {
-        validate(paymentKey, orderId, amount, requestedAt, approvedAt);
+                              final Long amount, final String requestedAt, final String approvedAt) {
+        validate(paymentKey, orderId, amount);
         return new Payment(id, paymentKey, orderId, amount, requestedAt, approvedAt);
     }
 
     public static Payment withId(final Long id, final String paymentKey, final String orderId,
-        final Long amount, final String requestedAt, final String approvedAt) {
+                                 final Long amount, final String requestedAt, final String approvedAt) {
         return of(id, paymentKey, orderId, amount, requestedAt, approvedAt);
     }
 
     public static Payment withoutId(final String paymentKey, final String orderId, final Long amount,
-        final String requestedAt, final String approvedAt) {
+                                    final String requestedAt, final String approvedAt) {
         return of(null, paymentKey, orderId, amount, requestedAt, approvedAt);
     }
 
-    private static void validate(final String paymentKey, final String orderId,
-        final Long amount, final String requestedAt, final String approvedAt) {
+    private static void validate(
+            final String paymentKey,
+            final String orderId,
+            final Long amount
+    ) {
         Validator.of(Payment.class)
-            .notNullField(Payment.Fields.paymentKey, paymentKey)
-            .notNullField(Payment.Fields.orderId, orderId)
-            .notNullField(Payment.Fields.amount, amount)
-            .notNullField(Payment.Fields.requestedAt, requestedAt)
-            .notNullField(Payment.Fields.approvedAt, approvedAt);
+                .notNullField(Payment.Fields.paymentKey, paymentKey)
+                .notNullField(Payment.Fields.orderId, orderId)
+                .notNullField(Payment.Fields.amount, amount);
     }
 }
